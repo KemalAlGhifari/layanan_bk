5132fca74f5d8517d27128467a5f8089d204ba27
rizkyatm <rizkyatmajaatm@gmail.com>
2022-10-05 08:34:09 +0700

    thirteenth

--- landing.php
+++ landing.php
@@ -18,6 +18,8 @@ $query2 = mysqli_query($koneksi,$sql2);
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css2?family=Archivo&family=Varta:wght@700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
 </head>
 <body>
@@ -81,10 +83,20 @@ $query2 = mysqli_query($koneksi,$sql2);
           <?php } ?>
             <!-- php end -->
             
-    
-      
+          </div>
         </div>
-      </div>
-    </div>   
+      </div>  
+      
+      <div class="footer">
+        <p>StarKonseling</p>
+        <p>StarKonseling merupakan website untuk membantu para murid SMK Taruna Bhakti dalam permasalahan didalam sekolah maupun diluar sekolah.</p>
+
+        <div class="list-icon">
+            <div><i class="fa-brands fa-facebook-f"></i></div>
+            <div><i class="fa-brands fa-whatsapp"></i></div>
+            <div><i class="fa-brands fa-instagram"></i></div>
+            <div><i class="fas fa-inbox"></i></div>
+        </div>
+    </div>
 </body>
 </html>
